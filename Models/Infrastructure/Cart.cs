3a4c6e25184c4e48046c62b490077ae47b8d87e8
Danil Kuzychenko <lymaaaan@gmail.com>
2023-12-21 17:51:58 +0400

    Feat: Another migrations...

--- Models/Infrastructure/Cart.cs
+++ Models/Infrastructure/Cart.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public User User { get; set; }
+        public int UserId { get; set; }
         public Product Product { get; set; }
         public int ProductId { get; set; }
     }
